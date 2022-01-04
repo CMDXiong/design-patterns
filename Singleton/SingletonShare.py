548265529bc287353e815d6bfa0d34c6460f2749
CMDXiong <1357211280@qq.com>
2022-01-04 23:41:04 +0800

    python 单例实现方式: 共享方式

--- Singleton/SingletonShare.py
+++ Singleton/SingletonShare.py
@@ -1,3 +1,20 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
+
+class Borg(object):
+    _state = {}
+
+    def __new__(cls, *args, **kw):
+        ob = super().__new__(cls, *args, **kw)
+        ob.__dict__ = cls._state
+        return ob
+
+
+class MyClass2(Borg):
+    a = 1
+
+
+sing = MyClass2()
+
+pass
