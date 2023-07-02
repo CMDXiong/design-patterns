029d82396d3f87370887b6442f4efde04af4abd2
CMDXiong <1357211280@qq.com>
2023-07-02 22:19:57 +0800

    装饰器模式

--- structural_pattern/decorator/decorator.py
+++ structural_pattern/decorator/decorator.py
@@ -1,10 +1,72 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
-""" 适配器模式（Adapter）"""
-""" 桥模式（Bridge）"""
-""" 组合模式（Composite）"""
-""" 装饰者模式（Decorator）"""
-""" 外观模式（Facade）"""
-""" 享元模式（Flyweight）"""
-""" 代理模式（Proxy）"""
+
+""" 装饰者模式（Decorator）
+装饰模式：动态地给一个对象增加一些额外的职责，就增加对象功能来说，装饰模式比生成子类实现更为灵活。
+其别名也可以称为包装器，与适配器模式的别名相同，但它们适用于不同的场合。根据翻译的不同，装饰模式也有人称之为“油漆工模式”。
+
+
+"""
+
+import abc
+
+
+class Component(abc.ABC):
+    def methodA(self):
+        pass
+
+    def methodB(self):
+        pass
+
+
+class ConcreteComponent(Component):
+    def methodA(self):
+        print("methodA")
+
+    def methodB(self):
+        print("methodB")
+
+
+class Decorator(Component):
+    def __init__(self, component):
+        self.component = component
+
+    def methodA(self):
+        pass
+
+    def methodC(self):
+        pass
+
+
+class ConcreteDecoratorA(Decorator):
+    def __init__(self, component):
+        super(ConcreteDecoratorA, self).__init__(component)
+
+    def methodA(self):
+        print("增强methodA")
+
+    def methodC(self):
+        print("增加methodC")
+
+
+class ConcreteDecoratorB(Decorator):
+    def __init__(self, component):
+        super(ConcreteDecoratorB, self).__init__(component)
+
+    def methodA(self):
+        print("增强methodA")
+
+    def methodB(self):
+        pass
+
+    def methodD(self):
+        print("增加methodD")
+
+
+if __name__ == "__main__":
+    component = ConcreteComponent()
+    component_c = ConcreteDecoratorA(component)
+
+    component_c.methodA()
+    component_c.methodC()
--- structural_pattern/decorator/demo.py
+++ structural_pattern/decorator/demo.py
@@ -1,3 +1,59 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
+"""
+一个完全不打扮的人，通过装饰戒指、耳环、项链提高颜值
+"""
+
+import abc
+
+
+class PersonBase(abc.ABC):
+    def get_beauty_value(self):
+        return 50
+
+
+class Person(PersonBase):
+    pass
+
+
+class Decorate(PersonBase):
+    def __init__(self, person):
+        self.person = person
+
+
+class RingDecorate(Decorate):
+    def __init__(self, person):
+        super(RingDecorate, self).__init__(person)
+
+    def get_beauty_value(self):
+        return self.person.get_beauty_value() + 10
+
+
+class NecklaceDecorator(Decorate):
+    def __init__(self, person):
+        super(NecklaceDecorator, self).__init__(person)
+
+    def get_beauty_value(self):
+        return self.person.get_beauty_value() + 20
+
+
+class EarringDecorator(Decorate):
+    def __init__(self, person):
+        super(EarringDecorator, self).__init__(person)
+
+    def get_beauty_value(self):
+        return self.person.get_beauty_value() + 30
+
+
+if __name__ == "__main__":
+    person = Person()
+    ring_person = RingDecorate(person)
+    print("使用戒指：", ring_person.get_beauty_value())
+
+    neck_ring_person = NecklaceDecorator(RingDecorate(person))
+    print("使用戒指、耳环的颜值：", neck_ring_person.get_beauty_value())
+
+    earr_neck_ring_person = EarringDecorator(NecklaceDecorator(RingDecorate(person)))
+    print("使用戒指、耳环、项链的颜值：", earr_neck_ring_person.get_beauty_value())
+
