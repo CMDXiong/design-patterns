c16c5cebd2c52e0457418f07775d5d152b853ae9
CMDXiong <1357211280@qq.com>
2023-07-02 22:47:11 +0800

    代理模式

--- structural_pattern/proxy/demo.py
+++ structural_pattern/proxy/demo.py
@@ -1,3 +1,50 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
+"""
+代理类在程序运行时创建的代理方式被成为动态代理
+
+可见，通过动态代理，极大简化了调用过程。
+
+相比于静态代理， 动态代理的优势在于可以很方便的对代理类的函数进行统一的处理，而不用修改每个代理类的函数
+"""
+
+import urllib.request as request
+import json
+
+
+def fetch_resource(resource_id):
+    """对于每一个REST操作，都会有类似的代码。差别仅在于API的地址和HTTP method（GET、POST、等）。
+    此时，可以引入一个GetProxy，可以代替我们实现这些繁杂的工作。"""
+    opener = request.urlopen('http://remote.server/api/resource/' + resource_id)
+    if opener.code != 200:
+        raise RuntimeError('invalid return code!')
+    content = opener.read()
+    try:
+        return json.loads(content)
+    except ValueError:
+        return content
+
+
+class GetProxy(object):
+    def __getattr__(self, api_path):
+        def _rest_fetch(*paras):
+            print(paras)
+            opener = request.urlopen('http://remote.server/api/' + api_path + '/' + '/'.join(paras))
+            if opener.code != 200:
+                raise RuntimeError('invalid return code!')
+            content = opener.read()
+            try:
+                return json.loads(content)
+            except ValueError:
+                return content
+
+        return _rest_fetch
+
+
+if __name__ == "__main__":
+    proxy = GetProxy()
+
+    # 调用API
+    proxy.user("123")  # http://remote.server/api/user/123
+    proxy.resource('switch', "456")  # http://remote.server/api/resource/switch/456
--- structural_pattern/proxy/proxy.py
+++ structural_pattern/proxy/proxy.py
@@ -1,10 +1,58 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
-""" 适配器模式（Adapter）"""
-""" 桥模式（Bridge）"""
-""" 组合模式（Composite）"""
-""" 装饰者模式（Decorator）"""
-""" 外观模式（Facade）"""
-""" 享元模式（Flyweight）"""
-""" 代理模式（Proxy）"""
+""" 代理模式（Proxy）
+代理模式：给某一个对象提供一个代理，并由代理对象控制对原对象的引用。
+
+代理类看起来和装饰模的写法很像，但两者的目的不同，
+装饰模式是为了增强功能或添加功能，代理模式主要是为了加以控制
+
+优点：
+    可以在不修改目标对象的前提下扩展目标对象的功能
+
+缺点：
+    如果需要代理多个类，每个类都会有一个代理类，会导致代理类无限制扩展；
+    如果类中有多个方法，同样的代理逻辑需要反复实现、应用到每个方法上，一旦接口增加方法，
+    目标对象与代理对象都要进行修改
+
+
+应用：
+    1. 打印日志，
+    2. 做权限管理。
+"""
+
+import abc
+
+
+class Subject(abc.ABC):
+    def do_something(self):
+        pass
+
+
+class RealSubject(Subject):
+    def do_something(self):
+        print("do something")
+
+
+class Proxy(Subject):
+    def __init__(self, real_subject):
+        self.real_subject = real_subject
+
+    def pre_handler(self):
+        print("pre_handler")
+
+    def post_handle(self):
+        print("post handler")
+
+    def do_something(self):
+        self.pre_handler()
+        self.real_subject.do_something()
+        self.pre_handler()
+
+
+if __name__ == "__main__":
+    real_subject = RealSubject()
+    proxy = Proxy(real_subject)
+    proxy.do_something()
+
+
