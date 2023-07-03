67634af894d0a6f747c9c4408ff7cd04c76a01b6
CMDXiong <1357211280@qq.com>
2023-07-03 18:40:01 +0800

    访问者模式

--- creational_pattern/prototype/prototype.py
+++ creational_pattern/prototype/prototype.py
@@ -7,24 +7,34 @@ import abc
 
 
 class PrototypeBase(abc.ABC):
-    def __init__(self):
-        pass
+    def __init__(self): pass
 
     @abc.abstractmethod
-    def clone(self, new_object):
-        pass
+    def clone(self): pass
 
 
 class Resume(PrototypeBase):
     def __init__(self, name, sex, age, worktime):
+        super(Resume, self).__init__()
         self.name = name
         self.sex = sex
         self.age = age
         self.worktime = worktime
 
-    def clone(self, new_object):
-        pass
+    def clone(self, ):
+        new_resume = Resume(self.name, self.sex, self.age, self.worktime)
+        return new_resume
+
+    def display(self):
+        print(self.name, self.sex, self.age, self.worktime)
 
 
 if __name__ == "__main__":
-    pass
+    resume = Resume("px", "fale", "18", "10")
+    resume.display()
+
+    clone_resume = resume.clone()
+    clone_resume.display()
+
+
+
